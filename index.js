8f7b0a77bbc80ddbaee23235f5fcdba45cf330fb
hayden ong <hayden.ong@outlook.com>
2017-07-25 15:26:42 +0800

    mc link3

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ $(function () {
         event.preventDefault();
 
         var sendData = {};
-        $(event.target.nodeName + ' :input').each(function(){
+        $(event.target.nodeName + ' :input').each(function () {
             sendData[this.name] = $(this).val();
         });
 
         var posting = $.ajax({
             type: 'POST',
-            url: $(event.target.nodeName).prop('action'),
-            data: sendData
+            url: '//anisland.us16.list-manage.com/subscribe/post?u=c1e7bdef38644dcf234cc05d7&amp;id=94e9eed170&c=?',
+            data: $('form').serialize(),
+            dataType: 'jsonp'
         });
 
         posting.done(function (response) {
             console.log(response);
             $('#alert-id').prop('hidden', false);
-            $('form :input').each(function(){
+            $('form :input').each(function () {
                 $(this).val('');
             })
         });
